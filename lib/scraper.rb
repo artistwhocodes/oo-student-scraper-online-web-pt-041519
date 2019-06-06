5410beac5f147c721a0c82cb68aae53a2bd6a145
github username <github email address>
2019-06-06 13:04:31 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,52 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open(index_url))
+    students = [] #empty array to store our list
+    # iterate through page beacuse that's where profile_url, location and name is
+      page.css("div.roster-cards-container .student-card").collect do |card|
+        #while we iterate, push our need infor inside
+        students <<
+        {
+          profile_url: card.css("a").attribute('href').value, #key => value
+          location: card.css('.student-location').text,
+          name: card.css('.student-name').text
+        }
+      end
+        students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student_profile = Nokogiri::HTML(open(profile_url))
+    profile_pages = {} #empty hash
+    student_profile.css("div.social-icon-container a").each do |link|
+      href = link.attribute("href").value  #all the links
+
+      if href.include?("twitter")  # find link that has twitter
+        profile_pages[:twitter] = href  #key will equal the link that has twitter
+      elsif href.include?("linkedin")
+        profile_pages[:linkedin] = href
+      elsif href.include?("github")
+        profile_pages[:github] = href
+      else
+        profile_pages[:blog] = href
+      end
+
+
+     profile_pages[:profile_quote] = student_profile.css("div.profile-quote").text
+     profile_pages[:bio] = student_profile.css("p").text
+        # :github =>           ,
+        # :blog =>             ,
+        # :profile_quote =>       ,
+        # :bio =>
+#student_profile.css(".social-icon-container a").attribute('href').value
+   end
+   profile_pages #return the now filled hash
+ # binding.pry
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,48 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each{|student| Student.new(student)}
   end
 
-  def add_student_attributes(attributes_hash)
-    
+  def add_student_attributes(attributes_hash) #instance variables assign to key,value
+    attributes_hash.each do |key,value|
+      if key == :twitter
+        @twitter = value
+      elsif
+        key == :linkedin
+        @linkedin = value
+      elsif
+        key == :github
+        @github = value
+      elsif
+        key == :blog
+        @blog = value
+      elsif
+        key == :profile_quote
+        @profile_quote = value
+      elsif
+        key == :profile_url
+        @profile_url = value
+      elsif
+        key == :bio
+        @bio = value
+      end
+    end
+   # binding.pry
   end
 
   def self.all
-    
+    @@all
   end
-end
 
+end
